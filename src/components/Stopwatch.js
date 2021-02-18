39bd5af1d5a0d14bd1b8c64137d8a78ebbdfbcce
shalynishka225 <shalynishka225@gmail.com>
2021-02-18 22:51:39 +0200

    chore: add debounce on onclick events instead of settimeout

--- src/components/Stopwatch.js
+++ src/components/Stopwatch.js
@@ -5,6 +5,7 @@ import {
   PauseOutlined,
   RedoOutlined,
 } from "@ant-design/icons";
+import { debounce } from "lodash";
 import "./Stopwatch.css";
 
 const Stopwatch = () => {
@@ -12,25 +13,25 @@ const Stopwatch = () => {
   const [isActive, setIsActive] = useState(false);
   const increment = useRef(null);
 
-  const handleStart = () => {
+  const handleStart = debounce(() => {
     if (!isActive) {
       setIsActive(true);
       increment.current = setInterval(() => {
         setTimer((timer) => timer + 1);
       }, 1000);
     }
-  };
+  }, 300);
 
-  const handlePause = () => {
+  const handlePause = debounce(() => {
     clearInterval(increment.current);
     setIsActive(false);
-  };
+  }, 300);
 
-  const handleReset = () => {
+  const handleReset = debounce(() => {
     clearInterval(increment.current);
     setIsActive(false);
     setTimer(0);
-  };
+  }, 300);
 
   const formatTime = () => {
     const getSeconds = `0${timer % 60}`.slice(-2);
@@ -46,31 +47,13 @@ const Stopwatch = () => {
       <Col span={12} offset={6}>
         <div className="stopwatch-body">
           <p>{formatTime()}</p>
-          <Button
-            onClick={() => {
-              setTimeout(() => {
-                handleStart();
-              }, 300);
-            }}
-          >
+          <Button onClick={handleStart}>
             <CaretRightOutlined />
           </Button>
-          <Button
-            onDoubleClick={() => {
-              setTimeout(() => {
-                handlePause();
-              }, 300);
-            }}
-          >
+          <Button onDoubleClick={handlePause}>
             <PauseOutlined />
           </Button>
-          <Button
-            onClick={() => {
-              setTimeout(() => {
-                handleReset();
-              }, 300);
-            }}
-          >
+          <Button onClick={handleReset}>
             <RedoOutlined />
           </Button>
         </div>
